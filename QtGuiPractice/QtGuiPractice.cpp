65f5b451f6856ba172219db8513eea81dee6f174
MingJing2015 <mjing01@mail.ubc.ca>
2023-06-07 22:57:28 -0700

    Second commit

--- QtGuiPractice/QtGuiPractice.cpp
+++ QtGuiPractice/QtGuiPractice.cpp
@@ -1,10 +1,10 @@
 #include "QtGuiPractice.h"
 #include <iostream>
 
-// will contain functionality of app 
-QtGuiPractice::QtGuiPractice(QWidget *parent)
-	: QMainWindow(parent)
+#include <QMessageBox>
 
+// will contain functionality of app 
+QtGuiPractice::QtGuiPractice(QWidget *parent) : QMainWindow(parent)
 {
 	ui.setupUi(this);
 	
@@ -21,6 +21,52 @@ QtGuiPractice::QtGuiPractice(QWidget *parent)
 	// this->setCentralWidget(ui.sampleTextBox);
 
 	setWindowTitle(QString("Qt GUI Practice"));
+
+
+	QSqlDatabase default_DB = QSqlDatabase::addDatabase("QSQLITE");
+
+	QStringList dbStringList = QSqlDatabase::connectionNames();
+
+	static const QString DEFAULT_HSS_DB_PathName = "./config_HSS.hss";
+
+	default_DB.setDatabaseName(DEFAULT_HSS_DB_PathName);
+
+	if (!default_DB.open()) {
+		QMessageBox::warning(nullptr, tr("WARNING"), QString("Can not open !"));
+	}
+
+	QSqlQuery	query;
+	QString		sql1 = "PRAGMA foreign_keys = ON;";
+	// QString		sql1 = "PRAGMA foreign_keys = OFF;";
+	if (!query.exec(sql1))
+		QMessageBox::critical(0, QObject::tr("Error: PRAGMA foreign_keys = ON;"), query.lastError().text());
+
+	// 1. projectInfo Table
+	QString sql = "CREATE TABLE ProjectInfo ("
+		"ProjectName	VARCHAR (50) PRIMARY KEY,"
+		"RecorderPath	VARCHAR (100),"
+		"Unit			VARCHAR (50),"
+		"Version		VARCHAR (20),"
+		"User			VARCHAR (50),"
+		"CreatedTime	TEXT,"
+		"Description	VARCHAR (100), "
+		"ModelType		VARCHAR (50) "
+		")";
+
+
+
+	if (!query.exec(sql))
+		QMessageBox::critical(0, QObject::tr("Error: create ProjectInfo table."), query.lastError().text());
+
+	sql = QString("insert into ProjectInfo (ProjectName, RecorderPath, Unit, Version, User, CreatedTime, Description, ModelType) VALUES('HSS project', '', 'Metric', '%1', '', '%2', 'HSS new project', '%3')")
+		.arg("1.0").arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm")).arg("2D");
+
+	if (!query.exec(sql))
+		qDebug() << query.lastError().text() << endl;
+
+
+	qDebug() << "test.. " << endl;
+
 
 
 }
